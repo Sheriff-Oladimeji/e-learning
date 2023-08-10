a1bb7bd1788ca84d1285074e2d61c4047a807c3c
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-10 12:09:14 +0100

    fix:modify filter buttons styles

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import allCourses from "../../data/courses/courses";
 import Button from "../button/Button";
 const Courses = () => {
-  const [courses, setcourses] = useState(allCourses);
+  const [courses, setCourses] = useState(allCourses);
 
-  const filterCategory = (category) => {
-    setcourses(allCourses.filter((course) => course.category === category));
-  };
+   const [selectedCategory, setSelectedCategory] = useState("All"); // Initialize with 'All'
+
+   const filterCategory = (category) => {
+     setSelectedCategory(category);
+     if (category === "All") {
+       setCourses(allCourses);
+     } else {
+       setCourses(allCourses.filter((course) => course.category === category));
+     }
+   }
   return (
     <section className="mb-20">
       <div className="flex flex-col md:flex-row  gap-10 justify-between">
@@ -14,13 +21,47 @@ const Courses = () => {
           Popular <span className="text-yellow">Courses</span>
         </h1>
         <div className="bg-[#E6EFFE] flex flex-wrap items-center gap-4 py-3 px-2 rounded-2xl">
-          <Button value="All" url="" onClick={() => setcourses(allCourses)} />
-         <button onClick={() => filterCategory("Design")}>Design</button>
-<button onClick={() => filterCategory("Development")}>Development</button>
-<button onClick={() => filterCategory("Business")}>Business</button>
-<button onClick={() => filterCategory("Marketing")}>Marketing</button>
-<button onClick={() => filterCategory("Data Science")}>Data Science</button>
-
+          <Button value="All" url="" onClick={() => setCourses(allCourses)} />
+          <button
+            onClick={() => filterCategory("Design")}
+            className={`${
+              selectedCategory === "Design" ? "bg-blue text-white" : ""
+            }  p-2 rounded-xl   border-none  text-center `}
+          >
+            Design
+          </button>
+          <button
+            onClick={() => filterCategory("Development")}
+            className={`${
+              selectedCategory === "Development" ? "bg-blue text-white" : ""
+            }  p-2 rounded-xl px-6   border-none  text-center `}
+          >
+            Development
+          </button>
+          <button
+            onClick={() => filterCategory("Business")}
+            className={`${
+              selectedCategory === "Business" ? "bg-blue text-white" : ""
+            }  p-2 rounded-xl   border-none  text-center `}
+          >
+            Business
+          </button>
+          <button
+            onClick={() => filterCategory("Marketing")}
+            className={`${
+              selectedCategory === "Marketing" ? "bg-blue text-white" : ""
+            }  p-2 rounded-xl   border-none  text-center `}
+          >
+            Marketing
+          </button>
+          <button
+            onClick={() => filterCategory("Data Science")}
+            className={`${
+              selectedCategory === "Data Science" ? "bg-blue text-white" : ""
+            }  p-2 rounded-xl  border-none  text-center `}
+          >
+            Data Science
+          </button>
         </div>
       </div>
 
@@ -57,8 +98,14 @@ const Courses = () => {
                   <h4 className="text-lg font-bold text-darkblue">
                     {course.price}
                   </h4>
-                  {course.initialPrice && <span className="text-lightslateblue line-through text-sm ">{course.initialPrice}</span>}
-                  {course.discount && <small className="text-xs">{course.discount}</small>}
+                  {course.initialPrice && (
+                    <span className="text-lightslateblue line-through text-sm ">
+                      {course.initialPrice}
+                    </span>
+                  )}
+                  {course.discount && (
+                    <small className="text-xs">{course.discount}</small>
+                  )}
                 </div>
                 <button className="bg-none outline-none border-none text-blue font-semibold">
                   Add to Cart
