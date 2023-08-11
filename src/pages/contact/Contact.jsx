898479baf0f8826590896db54e984f941b0b82ea
codebox124 <rajcodes733@gmail.com>
2023-08-11 13:49:45 -0700

    started the contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,12 @@
 import imgHeader from '../../assets/Title Backgrounds.png'
+import CardForm from '../../components/Contact/contact_form'
 import ContactInfo from '../../components/Contact/contact_info'
 const Contact = () => {
   return (
     <main className=' md:mt-4 w-[90%] mx-auto'>
       <img src={imgHeader} alt="" />
       <ContactInfo />
+      <CardForm />
       
 
     </main>
