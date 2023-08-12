5ad48b12cb135f5f366cea4b80940925a4752e79
codebox124 <rajcodes733@gmail.com>
2023-08-12 12:04:26 -0700

    updated the faq

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,14 @@
 import imgHeader from '../../assets/Title Backgrounds.png'
 import CardForm from '../../components/Contact/contact_form'
 import ContactInfo from '../../components/Contact/contact_info'
+import Faq from '../../components/Contact/faq'
 const Contact = () => {
   return (
     <main className=' md:mt-4 w-[90%] mx-auto'>
       <img src={imgHeader} alt="" />
       <ContactInfo />
       <CardForm />
+      <Faq />
       
 
     </main>
