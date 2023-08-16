4164168102811b6cfc4e08a3744c5ce3af0c7966
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-16 14:43:53 +0100

    feat: add about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,40 @@
-
+import double from "../../assets/double.png"
 import about from "../../data/about/about";
-
-
+import {TfiHeadphoneAlt} from "react-icons/tfi"
+import { BiMoviePlay } from "react-icons/bi";
+import { HiOutlineAcademicCap } from "react-icons/hi";
+const features = [
+  {
+    id: 1,
+    icon: <TfiHeadphoneAlt />,
+    title: "Expert Instructors",
+  },
+  {
+    id: 2,
+    icon: <BiMoviePlay />,
+    title: "2000+ Courses",
+  },
+  {
+    id: 3,
+    icon: <HiOutlineAcademicCap />,
+    title: "Educational Content",
+  },
+  {
+    id: 4,
+    icon: <TfiHeadphoneAlt />,
+    title: "Interactive Learning",
+  },
+  {
+    id: 5,
+    icon: <TfiHeadphoneAlt />,
+    title: "Extensive Support",
+  },
+  {
+    id: 6,
+    icon: <TfiHeadphoneAlt />,
+    title: "Interactive Learning",
+  },
+];
 
 const About = () => {
   return (
@@ -31,14 +64,41 @@ const About = () => {
             >
               <img src={item.icon} alt={item.icon} className="w-10" />
             </div>
-            <div >
+            <div>
               <h4 className="font-semibold mb-2  ">{item.title}</h4>
               <small className="text-lightslateblue">{item.desc}</small>
             </div>
           </div>
         ))}
       </main>
-      
+      <div className="mt-14 flex flex-col md:flex-row justify-between items-center  gap-20 md:gap-48">
+        <img src={double} alt="" className="h-[500px] object-contain" />
+        <div className="flex flex-col gap-8  ">
+          <h2 className="font-semibold text-2xl md:text-3xl text-darkslateblue">
+            Embrace <span className="text-yellow">Limitless</span> Learning
+            Opportunities, Accessible Anytime and{" "}
+            <span className="text-yellow">Anywhere</span>.
+          </h2>
+          <p className="text-lightslateblue text-xl">
+            Elevate your skills through our exceptional platform, featuring
+            world-class courses, certificates, and a vast repository of online
+            knowledge
+          </p>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-8  w-full">
+            {features.map((feature) => (
+              <div
+                key={feature.id}
+                className="flex justify-center flex-wrap  md:flex-nowrap gap-3 items-center"
+              >
+                <span className="bg-[#E6EFFE] p-2 rounded-full text-[#BD5C17]">
+                  {feature.icon}
+                </span>
+                <p className="text-darkblue text-center">{feature.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
