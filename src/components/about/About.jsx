85ee5d2efb8efa028d2c9ccf827ed26352fe2f4f
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-16 13:52:46 +0100

    fix: modify bugs

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -38,6 +38,7 @@ const About = () => {
           </div>
         ))}
       </main>
+      
     </section>
   );
 }
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,17 @@
 
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
 
-export default Cart
+
+
+
+  return (
+    <div className="mt-12 w-[90%] md:w-[80%] mx-auto font-mont">
+      <h1>Cart Page</h1>
+     
+     
+    </div>
+  );
+};
+
+export default Cart;
